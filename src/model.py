f9a2c5cd19493417440b593b819c022ef34b011e
Marius Kriegerowski <kriegero@uni-potsdam.de>
2018-08-06 12:23:48 +0200

    fix x, y, z errors and losses

--- src/model.py
+++ src/model.py
@@ -30,6 +30,7 @@ class Model(Object):
     summary_nth_step = Int.T(default=1)
     shuffle_size = Int.T(
         optional=True, help='if set, shuffle examples at given buffer size.')
+    tf.logging.set_verbosity(tf.logging.INFO)
 
     def __init__(self, tf_config=None, **kwargs):
         super().__init__(**kwargs)
@@ -59,33 +60,30 @@ class Model(Object):
     def generate_eval_dataset(self):
         ''' Generates evaluation data and labels.'''
         dataset = self.evaluation_data_generator.get_dataset()
-        if self.shuffle_size:
-            dataset = dataset.shuffle(buffer_size=self.shuffle_size)
-        return dataset.make_one_shot_iterator().get_next()
+        dataset = dataset.batch(self.batch_size)
+        return dataset
 
-    def time_axis_cnn(self, input, n_filters, cross_channel_kernel=None,
+    def time_axis_cnn(self, input, n_filters, kernel_height=None,
             kernel_width=1, name=None, training=False):
         '''
         CNN along horizontal axis
 
         :param n_filters: number of filters
-        :param cross_channel_kernel: convolution kernel size accross channels
+        :param kernel_height: convolution kernel size accross channels
         :param kernel_width: convolution kernel size accross time axis
 
         (Needs some debugging and checking)
-        TODO:
-         - cross_channel_kernel
         '''
         _, n_channels, n_samples, _ = input.shape
 
-        if cross_channel_kernel is None:
-            cross_channel_kernel = n_channels
+        if kernel_height is None:
+            kernel_height = n_channels
 
         with tf.variable_scope('conv_layer%s' %name):
             input = tf.layers.conv2d(
                 inputs=input,
                 filters=n_filters,
-                kernel_size=(cross_channel_kernel, kernel_width),    # use identity (1) along channels
+                kernel_size=(kernel_height, kernel_width),
                 activation=self.activation,
                 bias_initializer=self.initializer,
                 name=name+'conv2d')
@@ -93,7 +91,7 @@ class Model(Object):
             input = tf.layers.batch_normalization(input, training=training)
             input = tf.layers.max_pooling2d(
                 input,
-                pool_size=(cross_channel_kernel, kernel_width),       # (height, width)
+                pool_size=(kernel_height, kernel_width),
                 strides=(1, 2),
                 name=name+'max_pooling2d',
             )
@@ -104,56 +102,57 @@ class Model(Object):
                 tf.summary.image(
                     'post-%s' % name, tf.split(
                         input, num_or_size_splits=n_filters, axis=-1)[0])
-                variable_summaries(input, name)
+
         return input
 
     def model(self, features, labels, mode, params):
 
         training = bool(mode == tf.estimator.ModeKeys.TRAIN)
 
-        n_filters = params.get('base_capacity', 32)
+        n_filters = params.get('base_capacity', 8)
+        n_filters_factor = params.get('n_filters_factor', 2)
         kernel_width = params.get('kernel_width', 2)
         kernel_height = params.get('kernel_height', 2)
         kernel_width_factor = params.get('kernel_width_factor', 1)
         self.activation = params.get('activation', tf.nn.relu)
         n_channels, n_samples = self.data_generator.tensor_shape
         input = tf.reshape(features, [-1, n_channels, n_samples,  1])
+        if self.debug: 
+            view = features[:3]
+            view = tf.expand_dims(view, -1)
+            tf.summary.image('input', view)
 
-        # if self.debug: 
-        #     tf.summary.image('input', features)
-
-        for ilayer in range(params.get('n_layers', 3)):
-            input = self.time_axis_cnn(input, n_filters * (1 + ilayer),
-                    cross_channel_kernel=kernel_height,
+        for ilayer in range(params.get('n_layers', 2)):
+            n_filters = n_filters * (ilayer+1) * n_filters_factor
+            input = self.time_axis_cnn(input,
+                    n_filters=int(n_filters),
+                    kernel_height=kernel_height,
                     kernel_width=int(kernel_width + ilayer*kernel_width_factor),
                     name='conv_%s' % ilayer, training=training)
 
         fc = tf.contrib.layers.flatten(input)
-        fc = tf.layers.dense(fc, params.get('n_filters_dense', 512),
+        fc = tf.layers.dense(fc, params.get('n_filters_dense', 32),
                 activation=self.activation)
 
-        if params.get('dropout_rate', False):
+        dropout = params.get('dropout_rate', self.dropout_rate)
+        if dropout is not None:
             fc = tf.layers.dropout(
                 fc, rate=params['dropout_rate'], training=training)
-        elif self.dropout_rate:
-            fc = tf.layers.dropout(
-                fc, rate=self.dropout_rate, training=training)
 
         predictions = tf.layers.dense(fc, self.data_generator.n_classes)
         variable_summaries(predictions, 'predictions')
-
+        predictions = tf.transpose(predictions) 
+        labels = tf.transpose(labels)
         errors = predictions - labels
-        variable_summaries(tf.reduce_mean(errors[-1]), 'error_z')
-        variable_summaries(
-                tf.reduce_mean(
-                    tf.sqrt(tf.reduce_sum(errors[0: 1]**2, keepdims=False))),
-                'error_lateral')
 
-        loss = tf.reduce_mean(tf.sqrt(tf.reduce_sum((predictions - labels) ** 2, axis=1,
-            keepdims=False)))
-        loss = tf.Print(loss, [loss], "Mean Euclidian Error [m]: ")
-        # loss = tf.Print(loss, [tf.train.get_global_step()], "global step ")
-        variable_summaries(loss, 'training_loss')
+        variable_summaries(errors[0], 'error_x')
+        variable_summaries(errors[1], 'error_y')
+        variable_summaries(errors[2], 'error_z')
+
+        # loss_carthesian = tf.sqrt(tf.reduce_sum(errors ** 2, axis=1, keepdims=False))
+        # variable_summaries(loss_carthesian, 'training_loss')
+        # loss = tf.reduce_mean(loss_carthesian)
+        loss = tf.losses.mean_squared_error(labels, predictions)
 
         if mode == tf.estimator.ModeKeys.TRAIN:
             optimizer = tf.train.AdamOptimizer(
@@ -161,12 +160,19 @@ class Model(Object):
             train_op = optimizer.minimize(
                     loss=loss, global_step=tf.train.get_global_step())
 
+            logging_hook = tf.train.LoggingTensorHook(
+                    {"loss": loss, "step": tf.train.get_global_step()},
+                    every_n_iter=10)
+
             return tf.estimator.EstimatorSpec(
                     mode=mode,
                     loss=loss,
                     train_op=train_op,
                     # TODO: make this work for multi GPU:
-                    training_hooks=self.training_hooks)
+                    training_hooks=[
+                            self.get_summary_hook(),
+                            logging_hook,
+                        ])
 
         elif mode == tf.estimator.ModeKeys.EVAL:
             return tf.estimator.EstimatorSpec(mode=mode, loss=loss)
@@ -175,23 +181,31 @@ class Model(Object):
         return tf.train.SummarySaverHook(
             self.summary_nth_step,
             output_dir=self.summary_outdir,
-            summary_op=tf.summary.merge_all()
+            scaffold=tf.train.Scaffold(
+                summary_op=tf.summary.merge_all())
         )
 
     def train(self, params=None):
         params = params or {}
-        self.training_hooks = [self.get_summary_hook()]
 
         with self.sess as default:
             est = tf.estimator.Estimator(
                 model_fn=self.model, model_dir=self.outdir, params=params)
 
             est.train(input_fn=self.generate_dataset)
-            # est.train(input_fn=self.generate_input)
 
     def train_multi_gpu(self, params=None):
+        ''' Use multiple GPUs for training.  Buggy...
+        '''
         params = params or {}
-        with self.sess as default:
+        self.training_hooks = []
+        # saver_hook = tf.train.CheckpointSaverHook()
+        # summary_hook = tf.train.SummarySaverHook()
+        with tf.train.MonitoredSession(
+                # session_creator=tf.train.ChiefSessionCreator(),
+                # hooks=[summary_hook]) as sess:
+                # hooks=[saver_hook, summary_hook]) as sess:
+                ) as sess:
             distribution = tf.contrib.distribute.MirroredStrategy(num_gpus=2)
             run_config = tf.estimator.RunConfig(train_distribute=distribution)
             est = tf.estimator.Estimator(
