ddf4e8f524895a628da7a45b62cc77f15b66f9a9
Marius Kriegerowski <kriegero@uni-potsdam.de>
2018-08-13 17:57:16 +0200

    delete_if_exists for files

--- src/util.py
+++ src/util.py
@@ -5,10 +5,15 @@ import logging
 logger = logging.getLogger()
 
 
-def delete_if_exists(dirname):
-    if os.path.exists(dirname):
-        logger.info('deleting directory: %s' % dirname)
-        shutil.rmtree(dirname)
+def delete_if_exists(dir_or_file):
+    '''Deletes `dir_or_file` if exists'''
+    if os.path.exists(dir_or_file):
+        if os.path.isfile(dir_or_file):
+            logger.debug('deleting file: %s' % dir_or_file)
+            os.remove(dir_or_file)
+        else:
+            logger.debug('deleting directory: %s' % dir_or_file)
+            shutil.rmtree(dir_or_file)
 
 
 def nsl(tr):
