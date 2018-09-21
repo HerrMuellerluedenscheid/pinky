dfab1990ffd4c8e43cee5a0546607b276ce22d46
Marius Kriegerowski <marius.kriegerowski@gfz-potsdam.de>
2018-09-21 10:33:30 +0200

    wip separableCNN, process multiple configs

--- src/model.py
+++ src/model.py
@@ -23,7 +23,7 @@ logger = logging.getLogger('pinky.model')
 
 class Layer(Object):
     '''A 2D CNN followed by dropout and batch normalization'''
-    
+
     name = String.T(help='Identifies the model')
     n_filters = Int.T(help='Number of output filters')
     activation = String.T(
@@ -52,6 +52,7 @@ class CNNLayer(Layer):
     pool_height = Int.T()
 
     strides = Tuple.T(2, Int.T(), default=(1, 1))
+    tf_fun = tf.layers.conv2d
 
     def chain(self, input, training=False, dropout_rate=0.):
         _, n_channels, n_samples, _ = input.shape
@@ -59,7 +60,7 @@ class CNNLayer(Layer):
         logger.debug('input shape %s' % input.shape)
         kernel_height = self.kernel_height or n_channels
 
-        input = tf.layers.conv2d(
+        input = tf_fun(
             inputs=input,
             filters=self.n_filters,
             kernel_size=(kernel_height, self.kernel_width),
@@ -85,6 +86,10 @@ class CNNLayer(Layer):
         save_name = pjoin(save_path, 'kernel-%s' % self.name)
         weights = estimator.get_variable_value('%s/kernel' % self.name)
         plot.show_kernels(weights[:, :, index, ...], name=save_name)
+
+
+class SeparableCNNLayer(CNNLayer):
+    tf_fun = tf.layers.separable_conv2d
 
 
 class DenseLayer(Layer):
@@ -386,7 +391,7 @@ class Model(Object):
         ensure_dir(save_path)
         for layer in self.layers:
             plot.getActivations(layer, stimuli)
-        
+
     def save_model_in_summary(self):
         self.regularize()
         self.dump(filename=pjoin(self.get_summary_outdir(), 'model.config'))
@@ -398,6 +403,8 @@ def main():
     parser = argparse.ArgumentParser(
                 description='')
     parser.add_argument('--config')
+    parser.add_argument('--configs',
+            help='load a comma separated list of configs and process them')
     parser.add_argument('--train', action='store_true')
     parser.add_argument('--evaluate', action='store_true',
             help='Predict from input of `evaluation_data_generator` in config.')
@@ -432,98 +439,110 @@ def main():
         logging.basicConfig(stream=sys.stdout, level=logging.INFO)
         logger.setLevel(logging.INFO)
 
-    if args.config:
-        model = guts.load(filename=args.config)
-        model.config.setup()
-
-    if args.tfdebug:
-        model.enable_debugger()
-
-    if args.clear:
-        model.clear()
-
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth = True
     if args.gpu_no:
         tf_config.device_count = {'GPU': args.gpu_no}
-    model.set_tfconfig(tf_config)
 
-    if args.show_data:
-        from . import plot
-        plot.show_data(model, shuffle=True)
-        plt.show()
+    models = []
+    if args.config:
+        configs = [args.config]
+    configs.extend(args.configs)
 
-    elif args.write_tfrecord:
-        model_id = args.write_tfrecord
+    for config in configs:
+        model = guts.load(filename=config)
 
-        if os.path.isfile(args.write_tfrecord):
-            if args.force:
-                delete_candidate = guts.load(filename=args.write_tfrecord)
-                for g in [delete_candidate.config.evaluation_data_generator,
-                        delete_candidate.config.data_generator]:
-                    g.cleanup()
-                delete_if_exists(args.write_tfrecord)
-            else:
-                print('file %s exists. use --force to overwrite' % \
-                        args.write_tfrecord)
-                sys.exit(0)
+    for model in models:
+        logger.info('Start processing model: %s' model.name)
+        model.config.setup()
 
-        fn_tfrecord = '%s_train.tfrecord' % str(model_id)
-        tfrecord_data_generator = DataGeneratorBase(
-                fn_tfrecord=fn_tfrecord, config=model.config)
-        model.config.data_generator.write(fn_tfrecord)
-        model.config.data_generator = tfrecord_data_generator
+        if args.tfdebug:
+            model.enable_debugger()
 
-        fn_tfrecord = '%s_eval.tfrecord' % str(model_id)
-        tfrecord_data_generator = DataGeneratorBase(
-                fn_tfrecord=fn_tfrecord, config=model.config)
+        if args.clear:
+            model.clear()
+        model.set_tfconfig(tf_config)
 
-        model.config.evaluation_data_generator.write(fn_tfrecord)
-        model.config.evaluation_data_generator = tfrecord_data_generator
+        if args.show_data:
+            from . import plot
+            plot.show_data(model, shuffle=True)
+            plt.show()
 
-        model.name += '_tf'
-        model.dump(filename=args.write_tfrecord + '.config')
-        logger.info('Wrote new model file: %s' % (
-            args.write_tfrecord + '.config'))
+        elif args.write_tfrecord:
+            if len(models) != 1:
+                sys.exit('can only process one model at a time')
 
-    elif args.from_tfrecord:
-        logger.info('Reading data from %s' % args.from_tfrecord)
-        model.config.data_generator = TFRecordData(
-                fn_tfrecord=args.from_tfrecord)
+            model = models[0]
+            model_id = args.write_tfrecord
 
-    elif args.new_config:
-        fn_config = args.new_config
-        if os.path.exists(fn_config):
-            print('file exists: %s' % fn_config)
+            if os.path.isfile(args.write_tfrecord):
+                if args.force:
+                    delete_candidate = guts.load(filename=args.write_tfrecord)
+                    for g in [delete_candidate.config.evaluation_data_generator,
+                            delete_candidate.config.data_generator]:
+                        g.cleanup()
+                    delete_if_exists(args.write_tfrecord)
+                else:
+                    print('file %s exists. use --force to overwrite' % \
+                            args.write_tfrecord)
+                    sys.exit(0)
+
+            fn_tfrecord = '%s_train.tfrecord' % str(model_id)
+            tfrecord_data_generator = DataGeneratorBase(
+                    fn_tfrecord=fn_tfrecord, config=model.config)
+            model.config.data_generator.write(fn_tfrecord)
+            model.config.data_generator = tfrecord_data_generator
+
+            fn_tfrecord = '%s_eval.tfrecord' % str(model_id)
+            tfrecord_data_generator = DataGeneratorBase(
+                    fn_tfrecord=fn_tfrecord, config=model.config)
+
+            model.config.evaluation_data_generator.write(fn_tfrecord)
+            model.config.evaluation_data_generator = tfrecord_data_generator
+
+            model.name += '_tf'
+            model.dump(filename=args.write_tfrecord + '.config')
+            logger.info('Wrote new model file: %s' % (
+                args.write_tfrecord + '.config'))
+
+        elif args.from_tfrecord:
+            logger.info('Reading data from %s' % args.from_tfrecord)
+            model.config.data_generator = TFRecordData(
+                    fn_tfrecord=args.from_tfrecord)
+
+        elif args.new_config:
+            fn_config = args.new_config
+            if os.path.exists(fn_config):
+                print('file exists: %s' % fn_config)
+                sys.exit()
+
+            model = Model(
+                tf_config=tf_config,
+                data_generator=GFSwarmData.get_example())
+
+            model.regularize()
+
+        if args.train and args.optimize:
+            print('Can only use either --train or --optimize')
             sys.exit()
 
-        model = Model(
-            tf_config=tf_config,
-            data_generator=GFSwarmData.get_example())
+        if args.train:
+            if args.ngpu:
+                logger.info('Using %s GPUs' % args.ngpu)
+                model.train_multi_gpu()
+            else:
+                model.train_and_evaluate()
+                # model.train()
+        elif args.evaluate:
+            model.evaluate()
 
-        model.regularize()
+        elif args.predict:
+            # ATM this command should be named `evaluate`
+            model.predict()
 
-    if args.train and args.optimize:
-        print('Can only use either --train or --optimize')
-        sys.exit()
-
-    if args.train:
-        if args.ngpu:
-            logger.info('Using %s GPUs' % args.ngpu)
-            model.train_multi_gpu()
-        else:
-            model.train_and_evaluate()
-            # model.train()
-    elif args.evaluate:
-        model.evaluate()
-
-    elif args.predict:
-        # ATM this command should be named `evaluate`
-        model.predict()
-
-    elif args.optimize:
-        hyper_optimizer = guts.load(filename=args.optimize)
-        if args.clear:
-            hyper_optimizer.clear()
-        hyper_optimizer.optimize(model)
+        elif args.optimize:
+            hyper_optimizer = guts.load(filename=args.optimize)
+            if args.clear:
+                hyper_optimizer.clear()
+            hyper_optimizer.optimize(model)
 
